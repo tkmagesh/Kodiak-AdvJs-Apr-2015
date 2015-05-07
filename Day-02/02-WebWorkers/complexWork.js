ed789f02d7809b5a92595a6d914a0b2af4cbd7c2
tkmagesh <tkmagesh77@gmail.com>
2015-05-07 11:10:39 +0530

    Worker - Assignment

---
+++ Day-02/02-WebWorkers/complexWork.js
@@ -0,0 +1,12 @@
+console.log("worker script is loaded");
+function doWork(){
+    for(var i=0; i<10000; i++)
+        for(var j=0; j<10000; j++)
+            for(var k=0; k<100; k++){}
+}
+self.addEventListener("message", function(evtArg){
+    if (evtArg.data === "start"){
+        doWork();
+        self.postMessage("done");
+    }
+});
